aae8f479f841cf940183de72b104a19033618601
Virus019-LAB <ngunjugideon188@gmail.com>
2025-09-17 13:46:31 +0300

    Add files via upload

---
+++ my profile c.cpp
@@ -0,0 +1,26 @@
+// simple c program
+/*
+ name:gideon ngunju
+ reg:CT100/G/26133/25
+ des:showing my profile
+ */
+#include <stdio.h>
+
+//main function
+int main(int argc, char** argv)
+{
+	int height;
+	printf("enter your height:");
+	scanf("%d",&height );
+	printf("your height is %d \n", height);
+	int contact;
+	printf("enter your contact:");
+	scanf("%d",&contact);
+	printf("your contact is %d \n" , contact);
+	int weight;
+	printf("enter your weight:");
+	scanf("%d",&weight);
+	printf("your weight is %d \n", weight);
+	
+	return 0;
+}
