9ce25304d4a539dbe5a2a2009ebaccbb22badf08
Virus019-LAB <ngunjugideon188@gmail.com>
2025-09-18 09:57:43 +0300

    Add files via upload

---
+++ intro to prog 1.c
@@ -0,0 +1,23 @@
+//simple c program
+/*
+	name: GIDEON NGUNJU
+	reg: CT100/G/26133/25
+	description: semister units
+*/
+
+#include <stdio.h>
+
+//main function
+int main()
+{
+	printf("analogue electronics \n");
+	printf("computer system and organisation \n");
+	printf("fundementals of ICT \n");
+	printf("introduction to computer application \n");
+	printf("essentials of  accounting \n");
+	printf("communication skills \n");
+	printf("mathematics for science \n");
+	printf("introduction to programming and algorithms \n");
+	return 0;
+	
+}
